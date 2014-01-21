f1353850d0162fe051e7846640165f0a1d9809ef
Nacho <Nacho@Maquinon>
2014-01-21 20:24:22 +0100

    borrado de comentarios default

--- Escalador/src/datos/Itinerario.java
+++ Escalador/src/datos/Itinerario.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package datos;
 
 import java.io.Serializable;
@@ -9,7 +5,6 @@ import java.sql.Date;
 
 /**
  *
- * @author USUARIO
  */
 public class Itinerario implements Serializable{
     private String itinerario;
